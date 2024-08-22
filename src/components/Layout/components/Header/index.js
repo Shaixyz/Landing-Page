e15d699def5dcfe544eb58ecf207947f9b749b93
Kiet <kietquang240703@gmail.com>
2024-08-22 15:03:15 +0700

    Landing Page UI

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,118 +1,73 @@
-// Import
+import { Menu, X } from "lucide-react";
+import React from 'react';
+import { useState } from "react";
+import logo from "~/assets/logo.png";
+import { navItems } from "~/constants/index";
 
-import React from 'react'
-import classNames from "classnames/bind";
-import styles from './Header.module.scss'
-import { Link } from 'react-router-dom';
-import Glass from "~/assets/Glass.png"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
-import Avatar from "~/assets/Avatar.png"
-import CartIcon from "~/assets/Icon.png"
-import Dash from "~/assets/Icon2dash.png"
+const Header = () => {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
 
-import {
-  Drawer,
-  Button,
-  Typography,
-  IconButton,
-} from "@material-tailwind/react";
-//Function + variable
-
-const cx = classNames.bind(styles)
-
-
-function Header() {
-  const [open, Setopen] = useState(false);
-  const [search, Setsearch] = useState(false);
-  const [amount, Setamount] = useState(0);
-  const [account, Setaccount] = useState("Alan")
-  const bagIcon = document.getElementsByClassName(cx("shoppingWrapper"));
-  const avatarIcon = document.getElementsByClassName(cx("avatarWrapper relative mx-12 my-9 flex"));
-  const handlesearchClick = () => {
-    if (search === false) {
-      Setsearch(!search);
-      console.log(bagIcon[0])
-      console.log(avatarIcon[0])
-      bagIcon[0].style.display = 'none';
-      avatarIcon[0].style.display = "none";
-    } else {
-      Setsearch(!search);
-      bagIcon[0].style.display = "block";
-      avatarIcon[0].style.display = "block";
-    }
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
   };
-  //
 
-  // Start
   return (
-    <div className={cx("wrapper")}>
-      <div className={cx("dashIconWrapper")}>
-        <img src={Dash} className={cx("dashImg")} />
-      </div>
-      <div className={cx("navbarLink")}>
-        <Link
-          to="/confirm_order"
-          className={cx("findoutLink hover:underline mx-5 my-11")}
-        >
-          Find Out More
-        </Link>
-        <Link to="/" className={cx("aboutLink hover:underline mx-5 my-11 ")}>
-          About Us
-        </Link>
-        <Link to="/" className={cx("customLink hover:underline mx-5 my-11")}>
-          Custom Jeans
-        </Link>
-      </div>
-
-      <div className={cx("shopName")}>
-        <h2 className={cx("text-blue-900")}>JeanCraft</h2>
-      </div>
-
-      <div className={cx("action")}>
-        {/* Search Icon */}
-        <div className={cx("searchWrapper")}>
-          {search ? (
-            <div className={cx("inputWrapper")}>
-              <button className={cx("closeBtn")} onClick={handlesearchClick}>
-                <FontAwesomeIcon icon={faCircleXmark} />
-              </button>
-              <input
-                type="text"
-                className={cx("inputSearch bg-blue-400 h-8 w-21 rounded my-9")}
-              />
-            </div>
-          ) : (
-            <button
-              className={cx("search_btn")}
-              onClick={handlesearchClick}
-            >
-              <img src={Glass} className={cx("iconSearch w-6 ")} />
-            </button>
-          )}
-
-          {/* Shopping bag Icon */}
-          <div className={cx("shoppingWrapper mx-5 my-10")}>
-            <Link to="/following">
-              <img src={CartIcon} className={cx("iconShopping relative")} />
-              <p className={cx("shoppingAmount absolute top-0 my-8 mx-8 bg-orange-400 rounded-md w-3 text-center")}>{amount}</p>
-            </Link>
+    <nav className="sticky top-0 z-50 py-3 bg-black bg-opacity-50 backdrop-blur-lg border-b border-neutral-700/80">
+      <div className="container mx-auto px-4">
+      <div className="flex justify-between items-center max-w-screen-xl mx-auto">
+          <div className="flex items-center flex-shrink-0">
+            <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
+            <span className="text-xl text-white tracking-tight">Shai</span>
           </div>
-
-          <div className={cx("avatarWrapper relative mx-12 my-9 flex ")}>
-            <Link to ="/signin">
-              <img
-                src={Avatar}
-                className={cx("avatarIcon h-8 w-8 rounded-lg my-2 ")}
-              />
-            </Link>
-            <p className={cx("avatarHi mx-5 my-3")}>Hi,{account}</p>
+          <ul className="hidden text-white lg:flex ml-14 space-x-12">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <a href={item.href}>{item.label}</a>
+              </li>
+            ))}
+          </ul>
+          <div className="hidden text-white lg:flex justify-center space-x-12 items-center">
+            <a href="#" className="py-2 px-3 border rounded-md">
+              Sign In
+            </a>
+            <a
+              href="#"
+              className="bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md"
+            >
+              Create an account
+            </a>
+          </div>
+          <div className="lg:hidden text-white md:flex flex-col justify-end">
+            <button onClick={toggleNavbar}>
+              {mobileDrawerOpen ? <X /> : <Menu />}
+            </button>
           </div>
         </div>
+        {mobileDrawerOpen && (
+          <div className="fixed right-0 z-20 bg-neutral-900 w-full p-12 text-white flex flex-col justify-center items-center lg:hidden">
+            <ul>
+              {navItems.map((item, index) => (
+                <li key={index} className="py-4">
+                  <a href={item.href}>{item.label}</a>
+                </li>
+              ))}
+            </ul>
+            <div className="flex space-x-6">
+              <a href="#" className="py-2 px-3 border rounded-md">
+                Sign In
+              </a>
+              <a
+                href="#"
+                className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
+              >
+                Create an account
+              </a>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+    </nav>
   );
-}
+};
 
-export default Header
+export default Header;
